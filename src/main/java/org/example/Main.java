91fe3451bd4bfca6e66b3e36a2825a7c8b00a614
Mikhail Avdeev <avdeev.mikhail91@gmail.com>
2023-10-19 15:23:20 +0300

    add synchronized write file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,21 @@
 package org.example;
 
-import org.example.handlers.*;
+import org.example.handlers.AddHandler;
+import org.example.handlers.DeleteHandler;
+import org.example.handlers.GetHandler;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
     static Map<String, Queue<String[]>> queueMap = new ConcurrentHashMap<>();
-    static Map<String, String[]> fileFieldsMap = new HashMap<>();
-
 
     public static void main(String[] args) throws IOException {
         int port = 8008;
@@ -26,21 +29,22 @@ public class Main {
                 port = Integer.parseInt(args[0]);
         }
         Server server = new Server(port);
+        Map<String, String[]> fileFieldsMap = new HashMap<>();
         if (args.length > 2) {
             for (int i = 2; i < args.length; i++) {
                 File file = new File(args[i]);
-                init(file, delimiter);
+                init(file, delimiter, fileFieldsMap);
                 String name = file.getName().split("\\.")[0];
                 server.getServer().createContext("/get/%s".formatted(name), new GetHandler(queueMap.get(name),
                         fileFieldsMap.get(name)));
                 server.getServer().createContext("/add/%s".formatted(name), new AddHandler(queueMap.get(name),
-                        fileFieldsMap.get(name)));
+                        fileFieldsMap.get(name), file));
                 server.getServer().createContext("/delete/%s".formatted(name), new DeleteHandler(queueMap.get(name),
                         fileFieldsMap.get(name)));
             }
         } else {
             File file = new File(filename);
-            init(file, delimiter);
+            init(file, delimiter, fileFieldsMap);
             String name = file.getName().split("\\.")[0];
             server.getServer().createContext("/get/%s".formatted(name), new GetHandler(queueMap.get(name),
                     fileFieldsMap.get(name)));
@@ -48,16 +52,15 @@ public class Main {
         server.start();
     }
 
-    private static void init(File file, String delimiter) {
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+    private static void init(File file, String delimiter, Map<String, String[]> fileFieldsMap) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String s;
             String name = file.getName().split("\\.")[0];
-            Queue<String[]> queue = new ConcurrentLinkedQueue<>();
+            Queue<String[]> queue = new LinkedBlockingQueue<>();
             queueMap.put(name, queue);
             s = reader.readLine();
             fileFieldsMap.put(name, s.split(delimiter));
-            while ((s = reader.readLine()) != null) {
+            while ((s = reader.readLine()) != null && !s.equals("")) {
                 String[] split = s.split(delimiter);
                 queue.add(split);
             }
--- src/main/java/org/example/handlers/AddHandler.java
+++ src/main/java/org/example/handlers/AddHandler.java
@@ -3,10 +3,7 @@ package org.example.handlers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Collections;
 import java.util.Queue;
 
@@ -14,17 +11,24 @@ public class AddHandler implements HttpHandler {
 
     Queue<String[]> queue;
     String[] fieldsName;
+    File file;
+//    BufferedWriter writer;
+//    AtomicReference<BufferedWriter> atomicWriter = new AtomicReference<>();
 
-    public AddHandler(Queue<String[]> queue, String[] fieldsName) {
+    public AddHandler(Queue<String[]> queue, String[] fieldsName, File file) throws IOException {
         this.queue = queue;
         this.fieldsName = fieldsName;
+        this.file = file;
+//        this.writer = new BufferedWriter(new FileWriter(this.file, true));
+//        this.atomicWriter.set(new BufferedWriter(new FileWriter(this.file, true)));
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody()))) {
-            String[] s =  reader.readLine().split(";");
+            String[] s = reader.readLine().split(";");
             queue.add(s);
+            writeToFile(String.join(";", s));
         } catch (IOException | RuntimeException e) {
             String response = "Error";
             exchange.sendResponseHeaders(400, response.length());
@@ -45,5 +49,13 @@ public class AddHandler implements HttpHandler {
         os.write(response.getBytes());
         os.flush();
         os.close();
+    }
+
+    private synchronized void writeToFile(String str) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(str + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
